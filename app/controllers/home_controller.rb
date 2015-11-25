a49fb66651a0c6e2073748f5b4f4c4fcd058937a
Nghiem <nguyennghiem1205@hotmail.com>
2015-11-25 11:37:14 +0700

    create homeController

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def login
+  end
+
+  def hashtag
+  end
+end
---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class HomeControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get login" do
+    get :login
+    assert_response :success
+  end
+
+  test "should get hashtag" do
+    get :hashtag
+    assert_response :success
+  end
+
+end
